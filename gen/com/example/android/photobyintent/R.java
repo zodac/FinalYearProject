390ebe2773126a969b9fabc515d4ee15429dd4b9
zodac <arouge110@msn.com>
2014-02-27 21:16:24 +0000

    11th February 2013 - Sheikh Mehdi
    KeyAnalyser v0.3 - Based on photobyintent project on developer.android website
    
    Changes:
    =======
    - Changed targetSdkVersion from 12 to 17.
    - Added "android:allowBackup="true"" to manifest.
    - Imageview & Videoviews removed.
    - "Take Pic S" & "Take Pic B" buttons removed.
    - Deprecated code (imageview/videoview/pic button methods & variables)
    removed.
    - Main & Land/Main switched from LinearLayout to RelativeLayout.
    - Landscape view now working.
    
    Bugs:
    ====
    - FOR loop crashes after x iterations with a NULL pointer error. Using a
    break to continue, but needs to be looked at. Possible help at #android-dev on freenode.
    
    Solved Bugs:
    ==========
    - Nothing.

--- gen/com/example/android/photobyintent/R.java
+++ gen/com/example/android/photobyintent/R.java
@@ -14,16 +14,11 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int btnIntend=0x7f050000;
-        public static final int btnIntendS=0x7f050001;
-        public static final int btnIntendV=0x7f050002;
-        public static final int btnProc=0x7f050003;
-        public static final int imageView1=0x7f050004;
-        public static final int videoView1=0x7f050005;
+        public static final int btnIntendV=0x7f050000;
+        public static final int btnProc=0x7f050001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
-        public static final int main2=0x7f030001;
     }
     public static final class string {
         public static final int album_name=0x7f040007;
--- src/com/example/android/photobyintent/PhotoIntentActivity.java
+++ src/com/example/android/photobyintent/PhotoIntentActivity.java
@@ -5,8 +5,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 /*
@@ -18,7 +16,6 @@ import org.opencv.core.Scalar;
 import org.opencv.highgui.Highgui;
 import org.opencv.imgproc.Imgproc;
 */
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -26,165 +23,23 @@ import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.content.res.AssetFileDescriptor;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.media.MediaMetadataRetriever;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.Toast;
-import android.widget.VideoView;
 
 public class PhotoIntentActivity extends Activity {
-	
-	private String appName = "KeyAnalyser";
-	private String storageFolder = "/" + appName + "_tmp"; // /KeyAnalyser_tmp
-	
-	private static final int ACTION_TAKE_PHOTO_B = 1;
-	private static final int ACTION_TAKE_PHOTO_S = 2;
-	private static final int ACTION_TAKE_VIDEO = 3;
 
-	private static final String BITMAP_STORAGE_KEY = "viewbitmap";
-	private static final String IMAGEVIEW_VISIBILITY_STORAGE_KEY = "imageviewvisibility";
-	private ImageView mImageView;
-	private Bitmap mImageBitmap;
-
-	private static final String VIDEO_STORAGE_KEY = "viewvideo";
-	private static final String VIDEOVIEW_VISIBILITY_STORAGE_KEY = "videoviewvisibility";
-	private VideoView mVideoView;
-	private Uri mVideoUri;
-
-	private String mCurrentPhotoPath;
-
-	private static final String JPEG_FILE_PREFIX = "IMG_";
-	private static final String JPEG_FILE_SUFFIX = ".jpg";
-
-	private AlbumStorageDirFactory mAlbumStorageDirFactory = null;
-
-	
-	/* Photo album for this application */
-	private String getAlbumName() {
-		return getString(R.string.album_name);
-	}
-	private File getAlbumDir() {
-		File storageDir = null;
-
-		if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
-			
-			storageDir = mAlbumStorageDirFactory.getAlbumStorageDir(getAlbumName());
-
-			if (storageDir != null) {
-				if (! storageDir.mkdirs()) {
-					if (! storageDir.exists()){
-						Log.d("CameraSample", "failed to create directory");
-						return null;
-					}
-				}
-			}
-			
-		} else {
-			Log.v(getString(R.string.app_name), "External storage is not mounted READ/WRITE.");
-		}
-		
-		return storageDir;
-	}
-	@SuppressLint("SimpleDateFormat")
-	private File createImageFile() throws IOException {
-		// Create an image file name
-		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-		String imageFileName = JPEG_FILE_PREFIX + timeStamp + "_";
-		File albumF = getAlbumDir();
-		File imageF = File.createTempFile(imageFileName, JPEG_FILE_SUFFIX, albumF);
-		return imageF;
-	}
-	private File setUpPhotoFile() throws IOException {
-		
-		File f = createImageFile();
-		mCurrentPhotoPath = f.getAbsolutePath();
-		
-		return f;
-	}
-	private void setPic() {
-
-		/* There isn't enough memory to open up more than a couple camera photos */
-		/* So pre-scale the target bitmap into which the file is decoded */
-
-		/* Get the size of the ImageView */
-		int targetW = mImageView.getWidth();
-		int targetH = mImageView.getHeight();
-
-		/* Get the size of the image */
-		BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-		bmOptions.inJustDecodeBounds = true;
-		BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
-		int photoW = bmOptions.outWidth;
-		int photoH = bmOptions.outHeight;
-		
-		/* Figure out which way needs to be reduced less */
-		int scaleFactor = 1;
-		if ((targetW > 0) || (targetH > 0)) {
-			scaleFactor = Math.min(photoW/targetW, photoH/targetH);	
-		}
-
-		/* Set bitmap options to scale the image decode target */
-		bmOptions.inJustDecodeBounds = false;
-		bmOptions.inSampleSize = scaleFactor;
-		bmOptions.inPurgeable = true;
-
-		/* Decode the JPEG file into a Bitmap */
-		Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
-		
-		/* Associate the Bitmap to the ImageView */
-		mImageView.setImageBitmap(bitmap);
-		mVideoUri = null;
-		mImageView.setVisibility(View.VISIBLE);
-		mVideoView.setVisibility(View.INVISIBLE);
-	}
-	private void galleryAddPic() {
-		    Intent mediaScanIntent = new Intent("android.intent.action.MEDIA_SCANNER_SCAN_FILE");
-			File f = new File(mCurrentPhotoPath);
-		    Uri contentUri = Uri.fromFile(f);
-		    mediaScanIntent.setData(contentUri);
-		    this.sendBroadcast(mediaScanIntent);
-	}
-	private void dispatchTakePictureIntent(int actionCode) {
-
-		Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-		switch(actionCode) {
-		case ACTION_TAKE_PHOTO_B:
-			File f = null;
-			
-			try {
-				f = setUpPhotoFile();
-				mCurrentPhotoPath = f.getAbsolutePath();
-				takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
-			} catch (IOException e) {
-				e.printStackTrace();
-				f = null;
-				mCurrentPhotoPath = null;
-			}
-			break;
-
-		default:
-			break;			
-		} // switch
-
-		startActivityForResult(takePictureIntent, actionCode);
-	}
 	private void dispatchTakeVideoIntent() {
 		Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
-		startActivityForResult(takeVideoIntent, ACTION_TAKE_VIDEO);
+		startActivityForResult(takeVideoIntent, 3);
 	}
 	
 	private void dispatchProcVideoIntent() throws IOException{
 		
-		//Activity.requestWindowFeature(Window.FEATURE_PROGRESS); 
 		//Variables
 		Toast toast = Toast.makeText(getApplicationContext(), "Processing complete!", Toast.LENGTH_SHORT);
 		int image = 1;
@@ -200,7 +55,7 @@ public class PhotoIntentActivity extends Activity {
 		
 		//Create folder to store images		
 		String extStorageDirectory = Environment.getExternalStorageDirectory().toString();
-		File newFolder = new File(extStorageDirectory + storageFolder + File.separator + "tmp_images");
+		File newFolder = new File(extStorageDirectory + File.separator + "KeyAnalyser_tmp" + File.separator + "images_tmp");
 		newFolder.mkdir();
 		
 		String value = vidFile.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
@@ -214,7 +69,7 @@ public class PhotoIntentActivity extends Activity {
 			{
 				ByteArrayOutputStream bytes = new ByteArrayOutputStream();
 				bmp.compress(Bitmap.CompressFormat.PNG, 100, bytes);
-				File f = new File(Environment.getExternalStorageDirectory() + storageFolder + File.separator + "tmp_images" + File.separator + String.format(Locale.ENGLISH, "%03d", image) + ".png");
+				File f = new File(Environment.getExternalStorageDirectory() + File.separator + "KeyAnalyser_tmp" + File.separator + "images_tmp" + File.separator + String.format(Locale.ENGLISH, "%03d", image) + ".png");
 				f.createNewFile();
 				FileOutputStream fo = new FileOutputStream(f);
 				bytes.writeTo(fo);
@@ -306,36 +161,16 @@ public class PhotoIntentActivity extends Activity {
 		toast.show(); //Confirm end of method
 	}
 
-	private void handleSmallCameraPhoto(Intent intent) {
-		Bundle extras = intent.getExtras();
-		mImageBitmap = (Bitmap) extras.get("data");
-		mImageView.setImageBitmap(mImageBitmap);
-		mVideoUri = null;
-		mImageView.setVisibility(View.VISIBLE);
-		mVideoView.setVisibility(View.INVISIBLE);
-	}
-	private void handleBigCameraPhoto() {
-
-		if (mCurrentPhotoPath != null) {
-			setPic();
-			galleryAddPic();
-			mCurrentPhotoPath = null;
-		}
-
-	}
 	private void handleCameraVideo(Intent intent) {
-		//Method to change storage location of video file
-			//Good luck
 		
 		String extStorageDirectory = Environment.getExternalStorageDirectory().toString();
-		File newFolder = new File(extStorageDirectory + storageFolder);
+		File newFolder = new File(extStorageDirectory + File.separator + "KeyAnalyser_tmp");
 		newFolder.mkdir();
 		
 		try {
 		    AssetFileDescriptor videoAsset = getContentResolver().openAssetFileDescriptor(intent.getData(), "r");
 		    FileInputStream fis = videoAsset.createInputStream();
-		    //File f = new File(Environment.getExternalStorageDirectory() + storageFolder + File.separator + "test_" + String.format(Locale.ENGLISH, "%03d", image) + ".png");
-		    File tmpFile = new File(Environment.getExternalStorageDirectory() + storageFolder + File.separator + "video.mp4"); //TODO: change filename to application name 
+		    File tmpFile = new File(Environment.getExternalStorageDirectory() + File.separator + "KeyAnalyser_tmp" + File.separator + "video.mp4"); 
 		    FileOutputStream fos = new FileOutputStream(tmpFile);
 
 		    byte[] buf = new byte[40960];
@@ -345,34 +180,18 @@ public class PhotoIntentActivity extends Activity {
 		    }       
 		    fis.close();
 		    fos.close();
-		 } catch (IOException io_e) {
-		    // Handle error?
-		  }
-		
-		mVideoUri = intent.getData();
-		mVideoView.setVideoURI(mVideoUri);
-		mImageBitmap = null;
-		mVideoView.setVisibility(View.VISIBLE);
-		mImageView.setVisibility(View.INVISIBLE);
+		 } catch (IOException io_e){
+			 io_e.printStackTrace();
+		 }
 	}
-	Button.OnClickListener mTakePicOnClickListener = 
-		new Button.OnClickListener() {
-		public void onClick(View v) {
-			dispatchTakePictureIntent(ACTION_TAKE_PHOTO_B);
-		}
-	};
-	Button.OnClickListener mTakePicSOnClickListener = 
-		new Button.OnClickListener() {
-		public void onClick(View v) {
-			dispatchTakePictureIntent(ACTION_TAKE_PHOTO_S);
-		}
-	};
+
 	Button.OnClickListener mTakeVidOnClickListener = 
 		new Button.OnClickListener() {
 		public void onClick(View v) {
 			dispatchTakeVideoIntent();
 		}
 	};
+	
 	Button.OnClickListener mProcVidOnClickListener = 
 			new Button.OnClickListener() {
 			
@@ -387,29 +206,9 @@ public class PhotoIntentActivity extends Activity {
 	};
 
 	/** Called when the activity is first created. */
-	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		
-		mImageView = (ImageView) findViewById(R.id.imageView1);
-		mVideoView = (VideoView) findViewById(R.id.videoView1);
-		mImageBitmap = null;
-		mVideoUri = null;
-
-		Button picBtn = (Button) findViewById(R.id.btnIntend);
-		setBtnListenerOrDisable( 
-				picBtn, 
-				mTakePicOnClickListener,
-				MediaStore.ACTION_IMAGE_CAPTURE
-		);
-
-		Button picSBtn = (Button) findViewById(R.id.btnIntendS);
-		setBtnListenerOrDisable( 
-				picSBtn, 
-				mTakePicSOnClickListener,
-				MediaStore.ACTION_IMAGE_CAPTURE
-		);
 
 		Button vidBtn = (Button) findViewById(R.id.btnIntendV);
 		setBtnListenerOrDisable( 
@@ -424,31 +223,12 @@ public class PhotoIntentActivity extends Activity {
 				mProcVidOnClickListener,
 				MediaStore.ACTION_VIDEO_CAPTURE
 		);
-		
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.FROYO) {
-			mAlbumStorageDirFactory = new FroyoAlbumDirFactory();
-		} else {
-			mAlbumStorageDirFactory = new BaseAlbumDirFactory();
-		}
 	}
-	@Override
+	
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		switch (requestCode) {
-		case ACTION_TAKE_PHOTO_B: {
-			if (resultCode == RESULT_OK) {
-				handleBigCameraPhoto();
-			}
-			break;
-		} // ACTION_TAKE_PHOTO_B
 
-		case ACTION_TAKE_PHOTO_S: {
-			if (resultCode == RESULT_OK) {
-				handleSmallCameraPhoto(data);
-			}
-			break;
-		} // ACTION_TAKE_PHOTO_S
-
-		case ACTION_TAKE_VIDEO: {
+		case 3: {
 			if (resultCode == RESULT_OK) {
 				handleCameraVideo(data);
 			}
@@ -456,50 +236,14 @@ public class PhotoIntentActivity extends Activity {
 		} // ACTION_TAKE_VIDEO
 		} // switch
 	}
-	/*
-	public void VideoProcess() {
-        // Do something in response to button
-		
-		//char* filename = "/mnt/sdcard/DCIM/01.jpg";
-		//cv::Mat image = imread(filename, 1);
-		
-		String filename = "/mnt/sdcard/DCIM/01.jpg";		
-		Bitmap bmap = BitmapFactory.decodeFile(filename);
-		Mat imgToProcess = null;
-		
-		Utils.bitmapToMat(bmap, imgToProcess);
-		
-		Imgproc.cvtColor(imgToProcess, imgToProcess, Imgproc.COLOR_BGR2GRAY);
-		Imgproc.cvtColor(imgToProcess, imgToProcess, Imgproc.COLOR_GRAY2RGBA, 4);
-		
-	    Utils.matToBitmap(imgToProcess, bmap);
-    }
-	*/
 	
 	// Some lifecycle callbacks so that the image can survive orientation change
-	@Override
 	protected void onSaveInstanceState(Bundle outState) {
-		outState.putParcelable(BITMAP_STORAGE_KEY, mImageBitmap);
-		outState.putParcelable(VIDEO_STORAGE_KEY, mVideoUri);
-		outState.putBoolean(IMAGEVIEW_VISIBILITY_STORAGE_KEY, (mImageBitmap != null) );
-		outState.putBoolean(VIDEOVIEW_VISIBILITY_STORAGE_KEY, (mVideoUri != null) );
 		super.onSaveInstanceState(outState);
 	}
-	@Override
+	
 	protected void onRestoreInstanceState(Bundle savedInstanceState) {
 		super.onRestoreInstanceState(savedInstanceState);
-		mImageBitmap = savedInstanceState.getParcelable(BITMAP_STORAGE_KEY);
-		mVideoUri = savedInstanceState.getParcelable(VIDEO_STORAGE_KEY);
-		mImageView.setImageBitmap(mImageBitmap);
-		mImageView.setVisibility(
-				savedInstanceState.getBoolean(IMAGEVIEW_VISIBILITY_STORAGE_KEY) ? 
-						ImageView.VISIBLE : ImageView.INVISIBLE
-		);
-		mVideoView.setVideoURI(mVideoUri);
-		mVideoView.setVisibility(
-				savedInstanceState.getBoolean(VIDEOVIEW_VISIBILITY_STORAGE_KEY) ? 
-						ImageView.VISIBLE : ImageView.INVISIBLE
-		);
 	}
 	
 	/*** Indicates whether the specified action can be used as an intent. This
@@ -507,13 +251,8 @@ public class PhotoIntentActivity extends Activity {
 	 * respond to an intent with the specified action. If no suitable package is
 	 * found, this method returns false.
 	 * http://android-developers.blogspot.com/2009/01/can-i-use-this-intent.html
-	 *
-	 * @param context The application's environment.
-	 * @param action The Intent action to check for availability.
-	 *
-	 * @return True if an Intent with the specified action can be sent and
-	 *         responded to, false otherwise.
 	 */
+	
 	public static boolean isIntentAvailable(Context context, String action) {
 		final PackageManager packageManager = context.getPackageManager();
 		final Intent intent = new Intent(action);
@@ -522,6 +261,7 @@ public class PhotoIntentActivity extends Activity {
 					PackageManager.MATCH_DEFAULT_ONLY);
 		return list.size() > 0;
 	}
+	
 	private void setBtnListenerOrDisable( 
 			Button btn, 
 			Button.OnClickListener onClickListener,
@@ -535,5 +275,4 @@ public class PhotoIntentActivity extends Activity {
 			btn.setClickable(false);
 		}
 	}
-
 }
